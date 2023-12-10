9c88b9161c65b9810197cbe875f1b07b3630ecfb
Duarte Silva <dmcsilva@criticalsoftware.com>
2023-12-11 00:22:05 +0100

    Improved test and nomenclature

--- feedback/tests/unit/test-handler.mjs
+++ feedback/tests/unit/test-handler.mjs
@@ -1,6 +1,6 @@
 'use strict';
 
-import { lambdaHandler, postFeedback } from '../../app.mjs';
+import { getFeedback, postFeedback } from '../../app.mjs';
 import { expect } from 'chai';
 
 var event, context;
@@ -12,36 +12,6 @@ describe('Tests index', function () {
             resource: "{proxy+}"
         }
 
-        const result = await lambdaHandler(event, context)
-
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
-
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("Hello Mr. Holmes!");
-    });
-
-    it('Should ask who is the user', async () => {
-        event = {
-            "body": "{\"message\": \"feedback\"}",
-            "resource": "/{proxy+}"}
-        
-        const result = await lambdaHandler(event, context)
-
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
-
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal(`Who is there?`);
-    });
-
-    it('Should give a confirmation of submission', async () => {
         const result = await postFeedback(event, context)
 
         expect(result).to.be.an('object');
@@ -51,6 +21,36 @@ describe('Tests index', function () {
         let response = JSON.parse(result.body);
 
         expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("Your feedback was received!!");
+        expect(response.message).to.be.equal("Hello Mr. Holmes! Your feedback has been received!");
+    });
+
+    it('Should ask who is the user', async () => {
+        event = {
+            "body": "{\"message\": \"feedback\"}",
+            "resource": "/{proxy+}"}
+        
+        const result = await postFeedback(event, context)
+
+        expect(result).to.be.an('object');
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.be.an('string');
+
+        let response = JSON.parse(result.body);
+
+        expect(response).to.be.an('object');
+        expect(response.message).to.include('Error:');
+    });
+
+    it('Should give a confirmation of submission', async () => {
+        const result = await getFeedback(event, context)
+
+        expect(result).to.be.an('object');
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.be.an('string');
+
+        let response = JSON.parse(result.body);
+
+        expect(response).to.be.an('object');
+        expect(response.message).to.be.equal("Feedback: This is your feedback.");
     });
 });
